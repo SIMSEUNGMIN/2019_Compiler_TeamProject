e95a3f5e1c7b56c2d1b207f98f14e091af97fe33
someonewho <bluedao2@naver.com>
2019-12-14 18:13:33 +0900

    array to .j

--- HW05_3/src/listener/main/BytecodeGenListenerHelper.java
+++ HW05_3/src/listener/main/BytecodeGenListenerHelper.java
@@ -15,17 +15,17 @@ import listener.main.SymbolTable;
 import listener.main.SymbolTable.VarInfo;
 
 public class BytecodeGenListenerHelper {
-	
+
 	// <boolean functions>
 	static boolean isFunDecl(MiniCParser.ProgramContext ctx, int i) {
 		return ctx.getChild(i).getChild(0) instanceof MiniCParser.Fun_declContext;
 	}
-	
+
 	// type_spec IDENT '[' ']'
 	static boolean isArrayParamDecl(ParamContext param) {
 		return param.getChildCount() == 4;
 	}
-	
+
 	// global vars
 	static int initVal(Var_declContext ctx) {
 		return Integer.parseInt(ctx.LITERAL().getText());
@@ -49,21 +49,21 @@ public class BytecodeGenListenerHelper {
 	static boolean isArrayDecl(Local_declContext ctx) {
 		return ctx.getChildCount() == 6;
 	}
-	
+
 	static boolean isDeclWithInit(Local_declContext ctx) {
 		return ctx.getChildCount() == 5 ;
 	}
-	
+
 	//함수의 반환형이 void인지 확인
 	static boolean isVoidF(Fun_declContext ctx) {
 		String funType = ctx.type_spec().getText();
-		
+
 		if(funType == "void") {
 			return true;
 		}
 		return false;
 	}
-	
+
 	static boolean isIntReturn(MiniCParser.Return_stmtContext ctx) {
 		return ctx.getChildCount() ==3;
 	}
@@ -72,7 +72,7 @@ public class BytecodeGenListenerHelper {
 	static boolean isVoidReturn(MiniCParser.Return_stmtContext ctx) {
 		return ctx.getChildCount() == 2;
 	}
-	
+
 	// <information extraction>
 	static String getStackSize(Fun_declContext ctx) {
 		return "32";
@@ -80,53 +80,53 @@ public class BytecodeGenListenerHelper {
 	static String getLocalVarSize(Fun_declContext ctx) {
 		return "32";
 	}
-	
+
 	// 타입 확인
 	static String getTypeText(Type_specContext typespec) {
 		String typeString = typespec.getText();
 		return typeString;
 	}
-	
-	
+
+
 	// params
 	static String getParamName(ParamContext param) {
 		String paramName = param.type_spec().getText();
 		return paramName;
 	}
-	
+
 	static String getParamTypesText(ParamsContext params) {
 		String typeText = "";
-		
+
 		for(int i = 0; i < params.param().size(); i++) {
 			MiniCParser.Type_specContext typespec = (MiniCParser.Type_specContext)  params.param(i).getChild(0);
 			typeText += getTypeText(typespec); // + ";";
 		}
 		return typeText;
 	}
-	
+
 	// 지역변수 이름 반환
 	static String getLocalVarName(Local_declContext local_decl) {
 		//지역변수 이름 반환
 		String localName = local_decl.IDENT().getText();
 		return localName;
 	}
-	
+
 	// 함수 이름 반환
 	static String getFunName(Fun_declContext ctx) {
 		String funName = ctx.children.get(1).getText();
 		return funName;
 	}
-	
+
 	// 함수 이름 찾음
 	static String getFunName(ExprContext ctx) {
 		String funName = ctx.children.get(0).getText();
 		return funName;
 	}
-	
+
 	static boolean noElse(If_stmtContext ctx) {
 		return ctx.getChildCount() < 6;
 	}
-	
+
 	// 클래스의 처음 생성자형태를 채워서 반환 (전역변수도 있을 때를 고려)
 	static String getFunProlog() {
 		// return ".class public Test .....
@@ -140,12 +140,12 @@ public class BytecodeGenListenerHelper {
 				+ "aload_0" + "\n"
 				+ "invokenonvirtual java/lang/Object/<init>()V" + "\n";
 	}
-	
+
 	static String getFunEnd() {
 		return "return" + "\n"
 				+ ".end method" + "\n";
 	}
-	
+
 	static String getCurrentClassName() {
 		return "Test";
 	}
---
+++ HW05_3/test.c
@@ -0,0 +1,5 @@
+void main() {
+   int index[2];
+   index[1] = 3;
+   return 0;
+}
